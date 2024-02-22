7c5de9c196798f0ef8d25ffd717616ae1063394c
nikhilesh500 <nikhil.500apps@gmail.com>
2024-02-22 19:06:34 +0530

    Intial commit

---
+++ src/App.vue
@@ -0,0 +1,105 @@
+
+<template>
+  <div class="squad-container">
+    <h1>SQUAD</h1>
+    <div class='props-container' >
+      <PropsComponent 
+      v-for="(item, index) in database" 
+        :name="item.name" 
+        :age="item.age" 
+        :plays="item.plays" 
+        :matches="item.matches"
+        :runs="item.runs"
+        :team="item.team"
+        :position="index"
+        :key="index"
+        v-on:deleteCard="deleteCard"
+  
+      />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import PropsComponent from './components/PropsComponent.vue';
+
+let database = ref([
+  {
+    jersey: 25,
+    name: 'Nikhil',
+    age: 22,
+    plays:'All Rounder',
+    matches: 113,
+    runs: 2000,
+    team: 'Chennai Super Kings'
+  },
+  {
+    jersey: 7,
+    name: 'Dhoni',
+    age: 42,
+    plays:'Batsmen(W)(C)',
+    matches: 213,
+    runs: 8000,
+    team: 'Chennai Super Kings'
+  },
+  {
+    jersey: 17,
+    name: 'Bhuvaneswar',
+    age: 22,
+    plays:'Bowler',
+    matches: 173,
+    runs: 672,
+    team: 'Sunrisers Hyderabad'
+  },
+  {
+    jersey: 18,
+    name: 'Kohli',
+    age: 32,
+    plays:'Batsmen',
+    matches: 310,
+    runs: 672,
+    team: 'Royal Challengers Bglr'
+  },
+  {
+    jersey: 45,
+    name: 'Rohit',
+    age: 32,
+    plays:'Batsmen',
+    matches: 310,
+    runs: 3672,
+    team: 'Mumbai Indians'
+  },
+  {
+    jersey: 77,
+    name: 'Gill',
+    age: 32,
+    plays:'Batsmen',
+    matches: 110,
+    runs: 1672,
+    team: 'Gujarat Titans'
+  }
+]);
+
+const deleteCard = (position) => {
+  // console.log(position,'deleteThis')
+  database.value.splice(position, 1);
+};
+  
+</script>
+<style scoped>
+.squad-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.props-container {
+  display: flex;
+  flex-wrap: wrap; /* Allow items to wrap to the next row */
+  justify-content: space-evenly; /* Center items horizontally */
+  width: fit-content; /* Limit the width to fit content */
+  column-gap: 30px; /* Adjust the gap between items */
+  margin: 0 auto;
+}
+</style>
---
+++ src/components/PropsComponent.vue
@@ -0,0 +1,78 @@
+<template>
+    <div class="body">
+        <div class="card" :style="{background: 'linear-gradient(to bottom right, ' + cardBackground + ', #ffffff80 99%)'}">
+            <div class="names">
+                <h2>Name: </h2>
+                <h1>{{ name }}</h1>
+            </div>
+            <div class="details">
+                <div>
+                    <h4><b>Age: </b>{{ age }}</h4>
+                    <h4><b>Plays: </b>{{ plays }}</h4>
+                    <h4><b>Matches Played: </b>{{ matches }}</h4>
+                </div>
+                <div>
+                    <h4><b>Runs Scored: </b>{{ runs }}</h4>
+                    <h4><b>Team: </b>{{ team }}</h4>
+                    <h4><b>Position: </b>{{ position }}</h4>
+                </div>
+            </div>
+            <button type="button" @click="$emit('deleteCard', position)">Delete</button>
+        </div>
+    </div>
+</template>
+<script setup>
+import { computed } from 'vue';
+
+    const props = defineProps({
+        name: String,
+        plays: String,
+        age: Number,
+        matches: Number,
+        runs: Number,
+        team: String,
+        position: Number,
+    });
+    const cardBackground = computed(() => {
+    if (props.team === 'Chennai Super Kings') {
+        return 'yellow'; 
+    } else if (props.team === 'Sunrisers Hyderabad') {
+        return 'orange'; 
+    } else if (props.team === 'Royal Challengers Bglr') {
+        return 'red'; 
+    }  else if (props.team === 'Mumbai Indians') {
+        return '#001aff'; 
+    }  else if (props.team === 'Gujarat Titans') {
+        return '#01102e'; 
+    } else {
+        return 'lightgray'; 
+    }
+});
+</script>
+<style>
+b {
+    font-weight: 450;
+}
+.card{
+    border: none;
+    border-radius: 15px;
+    padding: 30px;
+    margin: 20px auto;
+    box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.411);
+    color: #000000;
+}
+.names {
+    display: flex;
+    gap: 20px;
+}
+.names h2 {
+    font-size: 20px;
+    margin: 10px 0px;
+}
+.details {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 20px;
+}
+</style>
